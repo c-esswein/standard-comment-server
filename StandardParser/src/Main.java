fc78611fa0e3a5b4882ae539327326ee70f3fe8e
Philipp Kratzer <mail@p-kratzer.com>
2015-03-11 17:03:37 +0100

    First tests using Jsoup as HTML parsing lib. Works as expected.

--- StandardParser/src/Main.java
+++ StandardParser/src/Main.java
@@ -1,9 +1,23 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
-
+		
+		
+		try {
+			Document doc = Jsoup.connect("http://www.orf.at").get();
+			System.out.println(doc.getElementById("nnav"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Finish");
 	}
 
 }
