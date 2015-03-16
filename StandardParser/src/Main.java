9c9bcd7101799be2a049c6610f769dc02009e7ab
Philipp Kratzer <mail@p-kratzer.com>
2015-03-16 13:50:45 +0100

    Code snippet allows to load comments from website. Request headers are modified to simulate AJAX request (simple GET leads to 404)

--- StandardParser/src/Main.java
+++ StandardParser/src/Main.java
@@ -1,23 +1,64 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
-
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
+
+		test1();
+	}
+
+	public static void test1() {
 		try {
-			Document doc = Jsoup.connect("http://www.orf.at").get();
-			System.out.println(doc.getElementById("nnav"));
+
+			String url = "http://mobil.derstandard.at/forum/1/2000012787886?_=1426507619442";
+			 
+			URL obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+	 
+			// optional default is GET
+			con.setRequestMethod("GET");
+	 
+			//add request header
+			con.setRequestProperty("X-Requested-With", "XMLHttpRequest");
+	 
+			int responseCode = con.getResponseCode();
+			System.out.println("\nSending 'GET' request to URL : " + url);
+			System.out.println("Response Code : " + responseCode);
+	 
+			BufferedReader in = new BufferedReader(
+			        new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+	 
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine + "\n");
+			}
+			in.close();
+	 
+			//print result
+			System.out.println(response.toString());
+			
+			PrintWriter pw = new PrintWriter("C:\\temp\\out.html");
+			pw.print(response.toString());
+			pw.close();
+	 
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		System.out.println("Finish");
 	}
-
 }
