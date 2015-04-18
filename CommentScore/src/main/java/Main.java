b8fa8c368bfaa4ff4a1b37fd22bde43ceaef5ecb
Thilo Gorfer <avencore@gmail.com>
2015-04-18 16:54:16 +0200

    remove stopwords prior to calculate lexical diversity

--- CommentScore/src/main/java/Main.java
+++ CommentScore/src/main/java/Main.java
@@ -5,9 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 		String goodComment = "Das ist ein zufälliger Kommentar der richtig geschrieben ist und einen hohen Quality score haben sollte";
 		String badComment = "dieser komment isch ein schlechta kommi scheiße dreck";
-
+		String testComment = "Ich würde jeden der hier postet und sich als ich kenn mich aus benennt, einladen mit Griechen zu sprechen. Unterhalten Sie sich mit Griechen (jungen, alten). Die werden Ihnen sagen wie es lief mit der vorherigen Regierung. Jede Woche eine neue Steuer auf irgendetwas, Arbeitslosenentgelt auf € 350,- gekürzt (max. Zeit 3 Monate), Mindestlohn auf € 350.- gekürzt, usw. usf. Ich möchte Sie eindringlichst ersuchen mit normalen Griechen zu reden und fragen zu stellen. Dann kommen Sie neu auf die Welt und brauchen nicht mehr auf die Griechen einzu.....  Danke.";
+		
 		CommentScore score = new CommentScore(100, 50, 80, 20);
-		System.out.println(score.rateComment(badComment) + "%");
+		
+		String newString = testComment.replaceAll("[^a-zA-ZäöüÄÜÖß ]","");
+		System.out.println(newString);
+		System.out.println(score.rateComment(newString) + "%");
 
 	}
 }
--- CommentScore/src/main/java/checker/LexicalDiversity.java
+++ CommentScore/src/main/java/checker/LexicalDiversity.java
@@ -1,19 +1,46 @@
 package checker;
 
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 import java.util.Map.Entry;
 
 public class LexicalDiversity {
 	private Map<String, Integer> occurences;
+	private File file;
+	private Set<String> dict;
 
 	public LexicalDiversity() {
 		occurences = new HashMap<String, Integer>();
+		file = new File("src/main/resources/stopwords.txt");
+		dict = new HashSet<String>(100000);
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(file);
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		while (scanner.hasNextLine()) {
+			dict.add(scanner.nextLine().toLowerCase().trim());
+		}
+		scanner.close();
 	}
 
 	public float check(String string) {
-		String[] tokens = string.split("\\s+");
+		String[] split = string.split("\\s+");
+		List<String> tokens = new ArrayList<String>();
+		
+		for (String token : split) {
+			tokens.add(token);
+		}
+		
+		tokens.removeAll(dict);
 		occurences.clear();
 
 		for (String token : tokens) {
@@ -25,7 +52,7 @@ public class LexicalDiversity {
 			}
 		}
 
-		return (checkForSingleOcc(occurences) / tokens.length) * 100;
+		return (checkForSingleOcc(occurences) / tokens.size()) * 100;
 	}
 
 	private float checkForSingleOcc(Map<String, Integer> occurences) {
